81d2fc5c63a35ea8e0a1d4f0a01e2b99ed9abc6f
andriy-kolokolov <koloandre22@gmail..com>
2023-08-21 14:01:02 +0200

    optimize imports

--- src/main.js
+++ src/main.js
@@ -1,13 +1,9 @@
+import 'bootstrap';
 import './assets/main.scss'
-
 import { createApp } from 'vue'
 import App from './App.vue'
-import 'bootstrap/scss/bootstrap.scss'; // Import the Bootstrap CSS
-import 'bootstrap'; // Import Bootstrap's JavaScript (optional if you need tooltips, popovers, etc.)
-import router from './router'
+import router from './router';
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
