d33679c1431103c58a0829888e59de8a868b5539
StefanoGalandrini <stefe68@gmail.com>
2023-08-22 15:52:01 +0200

    Add Show page with router link in Home.vue

---
+++ src/components/Show.vue
@@ -0,0 +1,26 @@
+<script>
+import axios from "axios";
+import {store} from "../store";
+
+export default {
+	data() {
+		return {
+			store,
+			apartment: null,
+		};
+	},
+	created() {
+		const id = this.$route.params.id;
+		axios
+			.get(`${this.store.baseUrlApi}apartments/all/${id}`)
+			.then((response) => {
+				this.apartment = response.data;
+			});
+		console.log(this.apartment);
+	},
+};
+</script>
+
+<template></template>
+
+<style></style>
