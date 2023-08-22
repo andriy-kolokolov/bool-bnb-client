2a14e0484afe4a97c1993e8d47a2d58db3268ab9
StefanoGalandrini <stefe68@gmail.com>
2023-08-22 23:17:04 +0200

    Add data, image and map based on received data

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,56 +1,57 @@
 <script>
 import axios from "axios";
-import { store } from "../store";
+import {store} from "../store";
 export default {
-  data() {
-    return {
-      store,
-      arrApartments: [],
-    };
-  },
-  methods: {
-    getAppartments() {
-      axios.get(this.store.baseUrlApi + "apartments/all").then((response) => {
-        this.arrApartments = response.data;
-      });
-      console.log(this.arrAppartments);
-    },
-  },
-  created() {
-    this.getAppartments();
-  },
+	data() {
+		return {
+			store,
+			arrApartments: [],
+		};
+	},
+	methods: {
+		getApartments() {
+			axios.get(this.store.baseUrlApi + "apartments/all").then((response) => {
+				this.arrApartments = response.data;
+			});
+			console.log(this.arrApartments);
+		},
+	},
+	created() {
+		this.getApartments();
+	},
 };
 </script>
 
 <template>
-  <div 
-    v-for="apartment in arrApartments" :key="apartment.id" 
-    class="card" style="width: 18rem;"
-  >
-  <!-- <div>{{ apartment.images[0] }}</div> -->
-  <!-- SISTEMARE LA CHIAMATA -->
+	<router-link
+		v-for="apartment in arrApartments"
+		:key="apartment.id"
+		:to="`/show/${apartment.id}`"
+		class="card"
+		style="width: 18rem">
+		<!-- <div>{{ apartment.images[0] }}</div> -->
+		<!-- SISTEMARE LA CHIAMATA -->
 
-    
-    <div class="card-body">
-      <h5 class="card-title">NAME: {{ apartment.name }}</h5>
-      <p class="card-text">ROOMS: {{ apartment.rooms }}</p>
-    </div>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item">BEDS: {{ apartment.beds }}</li>
-      <li class="list-group-item">BATHROOMS: {{ apartment.bathrooms }}</li>
-      <li class="list-group-item">SQAURE METERS: {{ apartment.square_meters }}</li>
-      <li class="list-group-item">AVAILABLE: {{ apartment.is_available }}</li>
-      <li class="list-group-item">SPONSOR: {{ apartment.sponsor }}</li>
-      <li class="list-group-item">ZIP: {{ apartment.zip }}</li>
-      <li class="list-group-item">CITY: {{ apartment.city }}</li>
-      <li class="list-group-item">ADDRESS: {{ apartment.address }}</li>
-    </ul>
-    <div class="card-body">
-      <a href="#" class="card-link">GPS: {{ apartment.gps_coordinates }}</a>
-    </div>
-  </div>
+		<div class="card-body">
+			<h5 class="card-title">NAME: {{ apartment.name }}</h5>
+			<p class="card-text">ROOMS: {{ apartment.rooms }}</p>
+		</div>
+		<ul class="list-group list-group-flush">
+			<li class="list-group-item">BEDS: {{ apartment.beds }}</li>
+			<li class="list-group-item">BATHROOMS: {{ apartment.bathrooms }}</li>
+			<li class="list-group-item">
+				SQAURE METERS: {{ apartment.square_meters }}
+			</li>
+			<li class="list-group-item">AVAILABLE: {{ apartment.is_available }}</li>
+			<li class="list-group-item">SPONSOR: {{ apartment.sponsor }}</li>
+			<li class="list-group-item">ZIP: {{ apartment.zip }}</li>
+			<li class="list-group-item">CITY: {{ apartment.city }}</li>
+			<li class="list-group-item">ADDRESS: {{ apartment.address }}</li>
+		</ul>
+		<div class="card-body">
+			<a href="#" class="card-link">GPS: {{ apartment.gps_coordinates }}</a>
+		</div>
+	</router-link>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
