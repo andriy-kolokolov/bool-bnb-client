76012a7b6deb19f6226e7c670163a79b54693f8d
andriy-kolokolov <koloandre22@gmail.com>
2023-08-23 11:09:09 +0200

    rename imports

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from "../src/components/Home.vue";
-import About from "../src/components/About.vue";
-import Show from "../src/components/Show.vue";
+import Home from "../src/pages/Home.vue";
+import About from "../src/pages/About.vue";
+import Show from "../src/pages/Show.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
