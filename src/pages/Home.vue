d2ae037d9024971a17ee0d97f47eb6b39772b2c7
alessandropecchini99 <pecchini99@gmail.com>
2023-08-25 16:45:48 +0200

    init card style

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,13 @@
 <script>
 import axios from "axios";
 import { store } from "../store/store.js";
+import { ref } from "vue";
 export default {
   data() {
     return {
       store,
       arrApartments: [],
+      showImg: ref(-1),
     };
   },
   methods: {
@@ -23,29 +25,102 @@ export default {
 </script>
 
 <template>
-  <router-link
-    v-for="apartment in arrApartments"
-    :key="apartment.id"
-    :to="`/apartment/${apartment.id}`"
-    class="card"
-    style="width: 18rem"
-  >
-    <!-- <div>{{ apartment.images[0] }}</div> -->
-    <!-- SISTEMARE LA CHIAMATA -->
-
-    <div class="card-body">
-      <h5 class="card-title">NAME: {{ apartment.name }}</h5>
-      <p class="card-text">ROOMS: {{ apartment.rooms }}</p>
-    </div>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item">BEDS: {{ apartment.beds }}</li>
-      <li class="list-group-item">BATHROOMS: {{ apartment.bathrooms }}</li>
-      <li class="list-group-item">
-        SQAURE METERS: {{ apartment.square_meters }}
-      </li>
-      <li class="list-group-item">AVAILABLE: {{ apartment.is_available }}</li>
-    </ul>
-  </router-link>
+  <div class="style">
+    <router-link
+      :to="`/apartment/${apartment.id}`"
+      v-for="(apartment, index) in arrApartments"
+      :key="apartment.id"
+      class="routerstyle"
+      @mouseover="showImg = index"
+      @mouseout="showImg = ''"
+    >
+      <div class="apartment_cards">
+        <img
+          class="cover_img"
+          :src="apartment.images[0].image_path"
+          :alt="apartment.name"
+        />
+        <!-- <img
+          v-if="index === showImg"
+          :src="apartment.images[1].image_path"
+          :alt="apartment.name"
+        /> -->
+        <div class="info">
+          <h5>{{ apartment.name }}</h5>
+          <p>AVAILABLE: {{ apartment.is_available }}</p>
+          <p>SPONSOR: {{ apartment.is_sponsored }}</p>
+          <p>CITY: {{ apartment.addresses[0].city }}</p>
+        </div>
+      </div>
+    </router-link>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.style {
+  width: fit-content;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 4em;
+  align-content: center;
+  justify-content: center;
+  .routerstyle {
+    text-decoration: none;
+    .apartment_cards {
+      width: 15vw;
+      min-width: 300px;
+      height: 50vh;
+      display: flex;
+      position: relative;
+
+      &:hover img {
+        bottom: 100px;
+        transition: 0.5s ease;
+      }
+
+      // &:hover .info {
+      //   opacity: 1;
+      //   transition: 0.2s ease;
+      // }
+
+      img {
+        width: 100%;
+        height: 40vh;
+        border-radius: 20px;
+        object-fit: cover;
+        position: absolute;
+        bottom: 20px;
+        left: 0;
+        z-index: -100;
+        transition: 0.5s ease;
+      }
+
+      .info {
+        // position: absolute;
+        // bottom: 0;
+        // left: 0;
+        // opacity: 0;
+        width: 100%;
+        height: 20vh;
+        background-color: rgb(235, 235, 235);
+        border-bottom-left-radius: 20px;
+        border-bottom-right-radius: 20px;
+        align-self: end;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        z-index: 0;
+
+        p,
+        h5 {
+          margin: 0;
+          text-align: center;
+          color: rgb(0, 0, 0);
+          // transition: 0.2s ease;
+        }
+      }
+    }
+  }
+}
+</style>
