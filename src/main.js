9c317350c7a248c803070e72435f82779d1d8945
alessandropecchini99 <pecchini99@gmail.com>
2023-09-01 15:11:09 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -1,15 +1,12 @@
-import 'bootstrap';
-import './assets/main.scss';
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import auth from "@/store/auth.js";
+import "bootstrap";
+import "./assets/main.scss";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 import AOS from "aos";
 import "aos/dist/aos.css";
-
-auth.initialize(); // check if already logged in
 
 const app = createApp(App);
 app.use(router);
 app.use(AOS);
-app.mount('#app');
+app.mount("#app");
